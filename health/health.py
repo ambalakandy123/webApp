b39395c22fc846b34b316f33d9b9cca403a5dc41
ambalakandy123 <ranjith.ra28@gmail.com>
2020-06-30 20:13:46 +1000

    frontend layout
    layout and bootsrap setup is done.
    displaying content.

--- health/health.py
+++ health/health.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app=Flask(__name__)
 
+    {
+        'author':'Ranjith ak',
+        'title':'advice consultant',
+        'content':'first post',
+        'date_posted':'july 1,2020'
+    },
+    {
+        'author':'priya ak',
+        'title':'advice consultant',
+        'content':'second post',
+        'date_posted':'july 3,2020'
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello page!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return dir
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
