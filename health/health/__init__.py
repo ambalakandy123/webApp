ee408a05a6f522954f309faf797684ff1c0d7680
ambalakandy123 <ranjith.ra28@gmail.com>
2020-07-07 21:23:04 +1000

    flask-bcrypt
    used it for user validation.
    username and email check is next.

--- health/health/__init__.py
+++ health/health/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3f8b455db08d114196721008c8abc2a3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from health import routes
