74c17b682f300016ab18f4107b3b1cb19e06ed8d
ambalakandy123 <ranjith.ra28@gmail.com>
2020-07-14 18:52:02 +1000

    update user datails
    Validation Error need to work on it...msut check the imported validator class

--- health/health/forms.py
+++ health/health/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, email_validator
 from health.models import User 
@@ -11,12 +12,11 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-
-        user = User.query.filter_by(usernme=username.data).first()
+        user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('The username is already taken.Please try different one.')
-    def validate_username(self, email):
 
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('The username is already taken.Please try different one.')
@@ -27,3 +27,20 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators =[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min=2,)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+       if username.data != current_user.username:
+             user = User.query.filter_by(username=username.data).first()
+             if user:
+                 raise ValidationError('The username is already taken.Please try different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+             user = User.query.filter_by(email=email.data).first()
+             if user:
+                 raise ValidationError('The email is already taken.Please try different one.')
